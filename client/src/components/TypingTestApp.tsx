a09e425f02fffb1b2c32785da57fc63ca4348644
David R <99555747+GitExcited@users.noreply.github.com>
2025-07-14 21:10:29 -0400

    Le clavier apparait seuelement quand les tests commencent

--- client/src/components/TypingTestApp.tsx
+++ client/src/components/TypingTestApp.tsx
@@ -1,20 +1,26 @@
-import { TestingProvider } from "./testing/TestingProvider";
+import { TestingProvider, useTesting } from "./testing/TestingProvider";
 import AutomatedTestingControls from "./testing/AutomatedTestingControls";
 import SuggestionProvider from "./suggestions/SuggestionProvider";
 import TypingInterface from "./typing/TypingInterface";
 import VirtualKeyboard from "./keyboard/VirtualKeyboard";
 
+function AppContent() {
+  const { isAutomatedTesting } = useTesting();
 
+  return (
+    <SuggestionProvider>
+      <AutomatedTestingControls />
+      <TypingInterface />
+      {isAutomatedTesting && <VirtualKeyboard />}
+    </SuggestionProvider>
+  );
+}
 
 export default function TypingTestApp() {
   return (
     <div className="flex flex-col min-h-screen bg-[#f9f9f9] overflow-hidden">
       <TestingProvider>
-        <SuggestionProvider>
-          <AutomatedTestingControls />
-          <TypingInterface />
-          <VirtualKeyboard />
-        </SuggestionProvider>
+        <AppContent />
       </TestingProvider>
     </div>
   );
