78402a1c88fa6193cc80abfe16a5f7ea6d939470
Thomas Houlachi <thomas04@hotmail.ca>
2025-07-21 13:35:36 -0400

    Enlever le (-) de peux-tu...

--- client/src/data/predictionScenarios.ts
+++ client/src/data/predictionScenarios.ts
@@ -144,19 +144,19 @@ export const PREDICTION_SCENARIOS: PredictionScenario[] = [
   },
   {
     id: "sentence-3",
-    targetSentence: "Peux-tu m'apporter les clés quand tu descends",
-    words: ["Peux-tu", "m'apporter", "les", "clés", "quand", "tu", "descends"],
+    targetSentence: "Peux tu m'apporter les clés quand tu descends",
+    words: ["Peux tu", "m'apporter", "les", "clés", "quand", "tu", "descends"],
     category: "Neutral",
     difficulty: "medium",
     wordCompletions: [
         [ // Peux-tu
-            [["Peux-tu", "Peux", "peut"]],
-            [["Peux-tu", "Peux"]],
-            [["Peux-tu"]],
-            [["Peux-tu"]],
-            [["Peux-tu"]],
-            [["Peux-tu"]],
-            [["Peux-tu"]],
+            [["Peux tu", "Peux", "peut"]],
+            [["Peux tu", "Peux"]],
+            [["Peux tu"]],
+            [["Peux tu"]],
+            [["Peux tu"]],
+            [["Peux tu"]],
+            [["Peux tu"]],
         ],
         [ // m'apporter
             [["m'apporter", "apporter", "emporter"]],
